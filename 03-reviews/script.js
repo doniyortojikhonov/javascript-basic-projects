82443183c1a3d2a87c09e153bdce50539890a5ad
Doniyor Tojikhonov <tojikhonov.doniyor@gmail.com>
2022-06-05 11:49:55 +0900

    sixth commit

---
+++ 03-reviews/script.js
@@ -0,0 +1,38 @@
+const reviews = [
+  {
+    id: 1,
+    name: "doniyor tojikhonov",
+    job: "front end developer",
+    img: "https://res.cloudinary.com/dnimnmxyt/image/upload/v1654396655/person-1_akbnkj.jpg",
+    text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. In fuga
+    ducimus animi exercitationem officiis quam ratione dolorem optio
+    debitis aperiam animi exercitationem officiis animi officiis.`,
+  },
+  {
+    id: 2,
+    name: "sanjar djuraev",
+    job: "QA Engineer",
+    img: "https://res.cloudinary.com/dnimnmxyt/image/upload/v1654396659/person-2_nhixo0.jpg",
+    text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. In fuga
+    ducimus animi exercitationem officiis quam ratione dolorem optio
+    debitis aperiam animi exercitationem officiis animi officiis.`,
+  },
+  {
+    id: 3,
+    name: "jasur juraev",
+    job: "web developer",
+    img: "https://res.cloudinary.com/dnimnmxyt/image/upload/v1654396661/person-3_itskiw.jpg",
+    text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. In fuga
+    ducimus animi exercitationem officiis quam ratione dolorem optio
+    debitis aperiam animi exercitationem officiis animi officiis.`,
+  },
+  {
+    id: 4,
+    name: "doniyor tojikhonov",
+    job: "back end developer",
+    img: "https://res.cloudinary.com/dnimnmxyt/image/upload/v1654396663/person-4_ktyoju.jpg",
+    text: `Lorem ipsum dolor sit, amet consectetur adipisicing elit. In fuga
+    ducimus animi exercitationem officiis quam ratione dolorem optio
+    debitis aperiam animi exercitationem officiis animi officiis.`,
+  },
+];
